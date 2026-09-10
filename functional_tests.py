76d4ee12c4f4934e6a72da41f3899765d00cd25c
Margot Teunisse <margotteunisse@hotmail.com>
2026-09-10 14:40:30 +0200

    Expand functional test Check for search bar and search results

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,14 +19,30 @@ class NewVisitorTest(unittest.TestCase):
 
         ##They haven't played the game, but they like birds.
         ##They want to see if their favourite bird, the black-tailed godwit, is in the game.
+        ##They look for a search bar.
+        searchbox = self.browser.find_element(By.ID, "search")
+        self.assertEqual(searchbox.get_attribute("placeholder"), "Look for a bird...")
+
+        ##They would like to use the English name.
+        ##They therefore check whether the search bar allows English.
+        self.assertNotEqual("EN", searchbox.get_attribute("language"))
+
+        ##Since the language is not set to English, they try the scientific name.
+        searchbox.send_keys("Limosa limosa")
+        searchbox.send_keys(Keys.ENTER)
+
+        ##They expect only a single result.
+        results = self.browser.find_element(By.ID, "search_results")
+        self.assertEqual(results.size, 1)
+
         self.fail("Finish the test!")
 
         ##Now they want to check that the black-tailed godwit is represented accurately.
         ##They check that the bird:
         ##- lives in wetlands
         ##- nests on the ground
-        ##- eats bugs and grains
         ##- has a wingspan of 76 cm
+        ##- eats bugs and grains, but not berries, fish or rodents.
 
         ##They notice that it has a nest capacity of 2.
         ##That number doesn't tell them much; they need to see how this compares to other birds.
