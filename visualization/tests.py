11d1d6b18304a8a7fccd02c624fc3ddeb491fbb6
Margot Teunisse <margotteunisse@hotmail.com>
2026-09-10 12:31:19 +0200

    Map "/" to home view

--- visualization/tests.py
+++ visualization/tests.py
@@ -4,9 +4,7 @@ from visualization.views import home_page
 
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode("utf8")
-        self.assertIn("<title>Wingspan data visualization</title>", html)
-        self.assertTrue(html.startswith("<html>"))
-        self.assertTrue(html.endswith("</html>"))
+        response = self.client.get("/")
+        self.assertContains(response, "<title>Wingspan data visualization</title>")
+        self.assertContains(response, "<html>")
+        self.assertContains(response, "</html>")
