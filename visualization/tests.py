60727c8d7772f087749236edd4de084a71c0943c
Margot Teunisse <margotteunisse@hotmail.com>
2026-09-17 16:50:36 +0200

    Update unit tests & add case insensitivity Make unit tests check response context directly, i/o checking HTML; otherwise too much overlap with functional tests.

--- visualization/tests.py
+++ visualization/tests.py
@@ -23,7 +23,7 @@ class HomePageTest(TestCase):
         response = self.client.post("/",
                                     data={"scientific_name": black_tailed_godwit.scientific_name})
 
-        self.assertContains(response, black_tailed_godwit.scientific_name)
+        self.assertTrue(black_tailed_godwit.scientific_name in response.context['search_results'])
         self.assertTemplateUsed(response, "home.html")
 
     def test_can_retrieve_bird_by_partial_name(self):
@@ -34,7 +34,7 @@ class HomePageTest(TestCase):
         response = self.client.post("/",
                                     data={"scientific_name": "Limosa"})
 
-        self.assertContains(response, "Limosa limosa")
+        self.assertTrue("Limosa limosa" in response.context["search_results"])
         self.assertTemplateUsed(response, "home.html")
 
 
@@ -46,7 +46,18 @@ class HomePageTest(TestCase):
         response = self.client.post("/",
                                     data={"scientific_name": peregrine_falcon.scientific_name})
 
-        self.assertContains(response, "Falco peregrinus")
+        self.assertTrue("Falco peregrinus" in response.context['search_results'])
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_bird_retrieval_is_case_insensitive(self):
+        peregrine_falcon = Bird()
+        peregrine_falcon.scientific_name = "Falco peregrinus"
+        peregrine_falcon.save()
+
+        response = self.client.post("/",
+                                    data={"scientific_name": "fAlCO"})
+
+        self.assertTrue("Falco peregrinus" in response.context['search_results'])
         self.assertTemplateUsed(response, "home.html")
 
 class BirdModelTest(TestCase):
--- visualization/views.py
+++ visualization/views.py
@@ -9,5 +9,6 @@ def home_page(request):
     query = request.POST.get("scientific_name")
     return render(request, "home.html",
                   {"search_results": ([bird.scientific_name for bird in Bird.objects.all()
-                                       if op.contains(bird.scientific_name, query)] if query is not None
+                                       if op.contains(bird.scientific_name.upper(), query.upper())]
+                                      if query is not None
                                       else [])})
