e238c9e5bf5394940adb5e8c5af1fdd63fd58f03
Margot Teunisse <margotteunisse@hotmail.com>
2026-09-19 13:17:56 +0200

    Start on charting area Write functional tests & add blank canvas to template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,27 @@
+import contextlib
+import os
+import shutil
+import tempfile
 import time
+from pathlib import Path
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from PIL import Image, ImageChops
 
 from visualization.models import Bird
 
+@contextlib.contextmanager
+def make_temp_directory():
+    temp_dir = tempfile.mkdtemp()
+    try:
+        yield temp_dir
+    finally:
+        shutil.rmtree(temp_dir)
 
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
@@ -25,15 +39,19 @@ class NewVisitorTest(StaticLiveServerTestCase):
         #Their browser windows is set to a specific size.
         self.browser.set_window_size(1024, 768)
 
-        #They notice the search box is centered.
+        #They notice that the search box and search results are aligned.
         searchbox = self.browser.find_element(By.ID,"search")
+        search_results = self.browser.find_element(By.ID, "results_list")
         self.assertAlmostEqual(searchbox.location["x"] + searchbox.size["width"]/2,
-                               512, delta=10)
+                               search_results.location["x"] + search_results.size["width"]/2, 0)
 
-        #They notice the results are centered as well.
-        searchbox = self.browser.find_element(By.ID, "results_list")
-        self.assertAlmostEqual(searchbox.location["x"] + searchbox.size["width"] / 2,
-                               512, delta=10)
+        #They notice that the search box is aligned with the selection menu for 1D plotting.
+        select_element = self.browser.find_element(By.NAME, "1D_plot_property_selection")
+        self.assertAlmostEqual(searchbox.location["y"], select_element.location["y"], -2)
+
+        #They notice that the 1D plotting selection menu is to the right of the search plot.
+        self.assertGreater(select_element.location["x"] - select_element.size["width"]/2,
+                           searchbox.location["x"] + searchbox.size["width"]/2)
 
     def test_can_look_up_birds(self):
         # Setup for test database
@@ -73,7 +91,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         searchbox.send_keys("Limosa limosa")
         searchbox.send_keys(Keys.ENTER)
 
-        # They wait max. 5 seconds for the results to load.
+        # They wait max. 1 second for the results to load.
         time.sleep(1)
 
         # They expect to see the black-tailed godwit as the only search result,
@@ -113,3 +131,50 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertTrue(all([result.text.startswith("Falco") for result in search_results]))
 
         # Satisfied with the game's collection of birds, they close the application.
+
+    def test_can_plot_1d_property_distribution(self):
+        # An avid Wingspan player visits the site.
+        # They would like to improve their strategy.
+        self.browser.get(self.live_server_url)
+
+        with make_temp_directory() as temp_dir_name:
+            temp_dir = Path(temp_dir_name)
+
+            # They notice a select menu that allows them to see how the selected property is distributed.
+            select_element = self.browser.find_element(By.NAME, "1D_plot_property_selection")
+            self.assertIn("Inspect property...", select_element.text)
+
+            # They also notice a blank charting area.
+            chart = self.browser.find_element(By.ID, "chart")
+            src_blank = str(temp_dir / "blank.png")
+            chart.screenshot(src_blank)
+
+            # They want to know how victory points are distributed over birds.
+            # They select the 'Victory points' property.
+            select = Select(select_element)
+            select.select_by_visible_text('Victory points')
+            time.sleep(1)
+
+            # Max. 1 second after making their selection, they see a chart appear.
+            chart = self.browser.find_element(By.ID, "chart")
+            src_victory_points = str(temp_dir / "victory_points.png")
+            chart.screenshot(src_victory_points)
+            diff = ImageChops.difference(Image.open(src_victory_points),
+                                    Image.open(src_blank))
+            self.assertIsNotNone(diff.getbbox())
+
+            # Satisfied with the victory points distribution, they also check the distribution of nest capacities.
+            #The chart changes visibly.
+            select.select_by_visible_text('Nest capacity')
+            time.sleep(1)
+            chart = self.browser.find_element(By.ID, "chart")
+            src_nest_capacity = str(temp_dir / "nest_capacity.png")
+            chart.screenshot(src_nest_capacity)
+            diff = ImageChops.difference(Image.open(src_nest_capacity),
+                                         Image.open(src_blank))
+            self.assertIsNotNone(diff.getbbox())
+            diff = ImageChops.difference(Image.open(src_nest_capacity),
+                                         Image.open(src_victory_points))
+            self.assertIsNotNone(diff.getbbox())
+
+        # Satisfied, the user closes the app.
